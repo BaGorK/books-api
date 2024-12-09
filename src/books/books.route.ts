fff06d570550a7f9046f35496b2ee43ca8eb48fd
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-12-09 12:55:36 +0300

    feat: implement authentication middleware and role-based access control for book routes

--- src/books/books.route.ts
+++ src/books/books.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { BooksRepository } from './providers/books.repository';
 import { BooksService } from './providers/books.service';
 import { BooksController } from './books.controller';
+import { AuthController } from './../auth/auth.controller';
+import { RoleEnum } from './../users/dtos/user.dto';
 
 const booksRepository = new BooksRepository();
 const booksService = new BooksService(booksRepository);
@@ -9,10 +11,14 @@ const booksController = new BooksController(booksService);
 
 const router = express.Router();
 
+router.use(AuthController.protect);
+
 router
   .route('/')
   .get((req, res) => booksController.getAllBook(req, res))
-  .post((req, res) => booksController.createBook(req, res));
+  .post(AuthController.restrictTo(RoleEnum.USER), (req, res) =>
+    booksController.createBook(req, res)
+  );
 
 router
   .route('/favorite')
@@ -21,7 +27,9 @@ router
 router
   .route('/:id')
   .get((req, res) => booksController.getBook(req, res))
-  .put((req, res) => booksController.updateBook(req, res))
-  .delete((req, res) => booksController.deleteBook(req, res));
+  .put(AuthController.restrictTo(RoleEnum.USER), (req, res) =>
+    booksController.updateBook(req, res)
+  )
+  .delete(AuthController.restrictTo(RoleEnum.USER, RoleEnum.ADMIN), (req, res) => booksController.deleteBook(req, res));
 
 export const bookRoutes = router;
